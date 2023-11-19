b957cb391893272cbe72a68218c9a251ae6bee5c
Orlando Ferreira <orlandofnfilho@gmail.com>
2023-11-19 13:17:54 -0300

    add CDI & ViewScoped cdi

--- PedidoVenda/src/main/java/com/algaworks/pedidovenda/controller/CadastroPedidoBean.java
+++ PedidoVenda/src/main/java/com/algaworks/pedidovenda/controller/CadastroPedidoBean.java
@@ -4,10 +4,10 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
 
-@ManagedBean
+@Named
 @RequestScoped
 public class CadastroPedidoBean implements Serializable{
 	private static final long serialVersionUID = 1L;
