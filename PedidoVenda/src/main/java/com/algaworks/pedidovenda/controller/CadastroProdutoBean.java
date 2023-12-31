37554a992bc053bd95fd5c7536d723a89d952d8d
Orlando Ferreira <orlandofnfilho@gmail.com>
2023-12-31 11:55:21 -0300

    adiciona Combo boxes dependentes Categorias

--- PedidoVenda/src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
+++ PedidoVenda/src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
@@ -11,6 +11,7 @@ import javax.validation.constraints.NotNull;
 import com.algaworks.pedidovenda.model.Categoria;
 import com.algaworks.pedidovenda.model.Produto;
 import com.algaworks.pedidovenda.repository.Categorias;
+import com.algaworks.pedidovenda.util.jsf.FacesUtil;
 
 @Named
 @ViewScoped
@@ -25,19 +26,25 @@ public class CadastroProdutoBean implements Serializable {
 	private Categoria categoriaPai;
 	
 	private List<Categoria> categoriasRaizes;
+	private List<Categoria> subcategorias;
 	
 	public CadastroProdutoBean() {
 		produto = new Produto();
 	}
 	
 	public void inicializar() {
-		System.out.println("Inicializando...");
-		
-		categoriasRaizes = categorias.raizes();
+		if (FacesUtil.isNotPostback()) {
+			categoriasRaizes = categorias.raizes();
+		}
+	}
+	
+	public void carregarSubcategorias() {
+		subcategorias = categorias.subcategoriasDe(categoriaPai);
 	}
 	
 	public void salvar() {
 		System.out.println("Categoria pai selecionada: " + categoriaPai.getDescricao());
+		System.out.println("Subcategoria selecionada: " + produto.getCategoria().getDescricao());
 	}
 
 	public Produto getProduto() {
@@ -55,6 +62,10 @@ public class CadastroProdutoBean implements Serializable {
 
 	public void setCategoriaPai(Categoria categoriaPai) {
 		this.categoriaPai = categoriaPai;
+	}
+
+	public List<Categoria> getSubcategorias() {
+		return subcategorias;
 	}
 
 }
