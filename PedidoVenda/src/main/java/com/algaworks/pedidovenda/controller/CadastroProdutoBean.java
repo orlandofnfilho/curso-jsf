65f9b70a7975a7bf9e761c65ac0e3dd2e7ef4922
Orlando Ferreira <orlandofnfilho@gmail.com>
2024-01-01 08:46:06 -0300

    adiciona Cadastro de Produtos e TransactionInterceptor

--- PedidoVenda/src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
+++ PedidoVenda/src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
@@ -1,6 +1,7 @@
 package com.algaworks.pedidovenda.controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ViewScoped;
@@ -11,6 +12,7 @@ import javax.validation.constraints.NotNull;
 import com.algaworks.pedidovenda.model.Categoria;
 import com.algaworks.pedidovenda.model.Produto;
 import com.algaworks.pedidovenda.repository.Categorias;
+import com.algaworks.pedidovenda.service.CadastroProdutoService;
 import com.algaworks.pedidovenda.util.jsf.FacesUtil;
 
 @Named
@@ -21,30 +23,41 @@ public class CadastroProdutoBean implements Serializable {
 
 	@Inject
 	private Categorias categorias;
-	
+
+	@Inject
+	private CadastroProdutoService cadastroProdutoService;
+
 	private Produto produto;
 	private Categoria categoriaPai;
-	
+
 	private List<Categoria> categoriasRaizes;
 	private List<Categoria> subcategorias;
-	
+
 	public CadastroProdutoBean() {
-		produto = new Produto();
+		limpar();
 	}
-	
+
 	public void inicializar() {
 		if (FacesUtil.isNotPostback()) {
 			categoriasRaizes = categorias.raizes();
 		}
 	}
-	
+
 	public void carregarSubcategorias() {
 		subcategorias = categorias.subcategoriasDe(categoriaPai);
 	}
-	
+
+	private void limpar() {
+		produto = new Produto();
+		categoriaPai = null;
+		subcategorias = new ArrayList<>();
+	}
+
 	public void salvar() {
-		System.out.println("Categoria pai selecionada: " + categoriaPai.getDescricao());
-		System.out.println("Subcategoria selecionada: " + produto.getCategoria().getDescricao());
+		this.produto = cadastroProdutoService.salvar(this.produto);
+		limpar();
+
+		FacesUtil.addInfoMessage("Produto salvo com sucesso!");
 	}
 
 	public Produto getProduto() {
