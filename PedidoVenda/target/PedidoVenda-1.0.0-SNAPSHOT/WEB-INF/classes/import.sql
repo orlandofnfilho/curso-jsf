8474daa0d5c5094b05caee3f0e5f593f10adff7a
Orlando Ferreira <orlandofnfilho@gmail.com>
2024-01-12 12:03:57 -0300

    Adiciona simple-mail by codylerum

--- PedidoVenda/target/PedidoVenda-1.0.0-SNAPSHOT/WEB-INF/classes/import.sql
+++
@@ -1,9 +0,0 @@
-insert into categoria (descricao) values ('Refrigerantes');
-insert into categoria (descricao) values ('Doces');
-insert into categoria (descricao) values ('Frutas');
-insert into categoria (descricao) values ('Bebidas Alcoolicas');
-insert into categoria (descricao) values ('Pereciveis');
-insert into categoria (descricao) values ('Frios');
-insert into produto (nome, sku, quantidade_estoque, valor_unitario, categoria_id) values ('Guaraná 2L', 'GUA00123', 10, 2.32, 1);
-insert into usuario (nome, email, senha) values ('Maria', 'maria@abadia.com', '123');
-insert into cliente (nome, email, tipo, doc_receita_federal) values ('Mercado do Tiao', 'mercado@tiao.com', 'JURIDICA', '10123123000101');
