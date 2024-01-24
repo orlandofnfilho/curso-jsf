6b65e5f5ee9ed9d7e89d5dd7b34c21707542c2b9
Orlando Ferreira <orlandofnfilho@gmail.com>
2024-01-24 03:25:55 -0300

    Adiciona consulta para o grafico de linhas

--- PedidoVenda/src/main/java/com/algaworks/pedidovenda/repository/Pedidos.java
+++ PedidoVenda/src/main/java/com/algaworks/pedidovenda/repository/Pedidos.java
@@ -1,19 +1,31 @@
 package com.algaworks.pedidovenda.repository;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateUtils;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.transform.Transformers;
+import org.hibernate.type.StandardBasicTypes;
+import org.hibernate.type.Type;
 
 import com.algaworks.pedidovenda.model.Pedido;
+import com.algaworks.pedidovenda.model.Usuario;
+import com.algaworks.pedidovenda.model.vo.DataValor;
 import com.algaworks.pedidovenda.repository.filter.PedidoFilter;
 
 public class Pedidos implements Serializable {
@@ -23,6 +35,59 @@ public class Pedidos implements Serializable {
 	@Inject
 	private EntityManager manager;
 
+	@SuppressWarnings({ "unchecked" })
+	public Map<Date, BigDecimal> valoresTotaisPorData(Integer numeroDeDias, Usuario criadoPor) {
+		Session session = manager.unwrap(Session.class);
+		
+		numeroDeDias -= 1;
+		
+		Calendar dataInicial = Calendar.getInstance();
+		dataInicial = DateUtils.truncate(dataInicial, Calendar.DAY_OF_MONTH);
+		dataInicial.add(Calendar.DAY_OF_MONTH, numeroDeDias * -1);
+		
+		Map<Date, BigDecimal> resultado = criarMapaVazio(numeroDeDias, dataInicial);
+		
+		Criteria criteria = session.createCriteria(Pedido.class);
+		
+		// select date(data_criacao) as data, sum(valor_total) as valor 
+		// from pedido where data_criacao >= :dataInicial and vendedor_id = :criadoPor 
+		// group by date(data_criacao)
+		
+		criteria.setProjection(Projections.projectionList()
+				.add(Projections.sqlGroupProjection("date(data_criacao) as data", 
+						"date(data_criacao)", new String[] { "data" }, 
+						new Type[] { StandardBasicTypes.DATE } ))
+				.add(Projections.sum("valorTotal").as("valor"))
+			)
+			.add(Restrictions.ge("dataCriacao", dataInicial.getTime()));
+		
+		if (criadoPor != null) {
+			criteria.add(Restrictions.eq("vendedor", criadoPor));
+		}
+		
+		List<DataValor> valoresPorData = criteria
+				.setResultTransformer(Transformers.aliasToBean(DataValor.class)).list();
+		
+		for (DataValor dataValor : valoresPorData) {
+			resultado.put(dataValor.getData(), dataValor.getValor());
+		}
+		
+		return resultado;
+	}
+
+	private Map<Date, BigDecimal> criarMapaVazio(Integer numeroDeDias,
+			Calendar dataInicial) {
+		dataInicial = (Calendar) dataInicial.clone();
+		Map<Date, BigDecimal> mapaInicial = new TreeMap<>();
+
+		for (int i = 0; i <= numeroDeDias; i++) {
+			mapaInicial.put(dataInicial.getTime(), BigDecimal.ZERO);
+			dataInicial.add(Calendar.DAY_OF_MONTH, 1);
+		}
+		
+		return mapaInicial;
+	}
+	
 	@SuppressWarnings("unchecked")
 	public List<Pedido> filtrados(PedidoFilter filtro) {
 		Session session = this.manager.unwrap(Session.class);
