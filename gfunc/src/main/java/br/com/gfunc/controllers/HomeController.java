8a22953b70cb9b5ba9973fc07300c82c4e6636d0
Orlando Ferreira <orlandofnfilho@gmail.com>
2024-03-16 18:13:02 -0300

    update

--- gfunc/src/main/java/br/com/gfunc/controllers/HomeController.java
+++ gfunc/src/main/java/br/com/gfunc/controllers/HomeController.java
@@ -5,8 +5,6 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
-import br.com.gfunc.services.exception.NegocioException;
-
 @ManagedBean
 @ViewScoped
 public class HomeController implements Serializable{
